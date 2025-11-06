1b1bcbadf730f6b539980dd28e0c16c04559f491
MatheusBerwaldt <mathsmachado18@gmail.com>
2025-11-06 14:24:04 -0300

    feat: Mudança no main.c

--- mymemory.h
+++ mymemory.h
@@ -3,21 +3,18 @@
 
 #include <stddef.h>
 
-// Estrutura que representa um bloco de memória alocado
 typedef struct allocation {
     void *start;
     size_t size;
     struct allocation *next;
 } allocation_t;
 
-// Estrutura que representa o pool total de memória
 typedef struct {
-    void *pool;              // ponteiro para o bloco de memória real
-    size_t total_size;       // tamanho total do pool
-    allocation_t *head;      // ponteiro para lista encadeada de alocações
+    void *pool;
+    size_t total_size;       
+    allocation_t *head;      
 } mymemory_t;
 
-// Funções principais
 mymemory_t* mymemory_init(size_t size);
 void* mymemory_alloc(mymemory_t *memory, size_t size);
 void mymemory_free(mymemory_t *memory, void *ptr);
